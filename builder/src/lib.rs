befa1ce502d1331dd7926b1db03db4ea230790d3
TakahisaI <ishitanee@gmail.com>
2023-04-24 11:57:20 +0900

    pass test01

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,9 @@
-use proc_macro::TokenStream;
+use quote::quote;
+use syn::DeriveInput;
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let _ = syn::parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    quote! {}.into()
 }
